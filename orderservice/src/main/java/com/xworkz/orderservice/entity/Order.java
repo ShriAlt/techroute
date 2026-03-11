88250fdbc88c190a5d10943eedb2c5635eff6d8a
Shriharsha K M <shriharshakm10@gmail.com>
2026-03-11 22:30:11 +0530

    Add auditing support with AuditorAwareImpl; create BaseEntity for common fields; implement JwtAuthenticationFilter and JwtUtil for JWT handling; define Order entity and OrderStatus enum

--- orderservice/src/main/java/com/xworkz/orderservice/entity/Order.java
+++ orderservice/src/main/java/com/xworkz/orderservice/entity/Order.java
@@ -1,11 +1,8 @@
 package com.xworkz.orderservice.entity;
 
-import com.xworkz.orderservice.enums.OrderStatus;
 import jakarta.persistence.*;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.UUID;
+
 
 @Entity
 @Table(name = "orders")
