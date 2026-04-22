6fe1b7bbf25c338d26a6c42c797d191de3a0752e
Shriharsha K M <shriharshakm10@gmail.com>
2026-04-22 12:47:36 +0530

    Add auditing support with AuditorAwareImpl; create BaseEntity for common fields; implement JwtAuthenticationFilter and JwtUtil for JWT handling; define Order entity and OrderStatus enum

--- orderservice/src/main/java/com/xworkz/orderservice/config/AuditorAwareImpl.java
+++ orderservice/src/main/java/com/xworkz/orderservice/config/AuditorAwareImpl.java
@@ -11,21 +11,21 @@ public class AuditorAwareImpl implements AuditorAware<String > {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-         Return logged-in username, or "SYSTEM" if none
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return Optional.of("SYSTEM"); // fallback if no user is logged in
-        }
-        Object principal = authentication.getPrincipal();
-        if (principal == null) {
-            return Optional.of("SYSTEM"); // avoid NPE
-        }
-
-        if (principal instanceof UserDetails userDetails) {
-            return Optional.of(userDetails.getUsername());
-        }
-
+//         Return logged-in username, or "SYSTEM" if none
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//
+//        if (authentication == null || !authentication.isAuthenticated()) {
+//            return Optional.of("SYSTEM"); // fallback if no user is logged in
+//        }
+//        Object principal = authentication.getPrincipal();
+//        if (principal == null) {
+//            return Optional.of("SYSTEM"); // avoid NPE
+//        }
+//
+//        if (principal instanceof UserDetails userDetails) {
+//            return Optional.of(userDetails.getUsername());
+//        }
+//
         return Optional.of("SYSTEM");
 
 
